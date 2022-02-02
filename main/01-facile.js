cf466ede470f4d7af8d00532465f395f3a8fa9fa
alizeesalvi <alizee.salvi@icloud.com>
2022-02-02 14:11:26 +0000

    <Alizée Salvi>

--- main/01-facile.js
+++ main/01-facile.js
@@ -5,13 +5,30 @@
  * - "NUL" s'il est nul
  */
 function signe(nombre) {
-
+    if (nombre > 0) {
+        return 'POSITIVE'
+    } else if (nombre < 0) {
+        return 'NEGATIVE'
+    } else {
+        return 'NUL'
+    }
 }
+console.log(signe(0))
 
 /**
  * Programmer une fonction prenant en argument un tableau de nombres, et qui renvoie un tableau ne contenant que les nombres pairs.
  * Astuce : un nombre pair est un nombre dont le reste de la division par 2 est 0.
  */
 function nombresPairs(nombres) {
-
+    var TabPAire = [];
+    var TabIndex = 0;
+    for (var nbre = 0; nbre < nombres.length; nbre++) {
+        if (nombres[nbre] % 2 === 0) {
+            TabPAire[TabIndex] = nombres[nbre];
+            TabIndex++;
+        }
+    }
+    return TabPAire
 }
+var MonTableau = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(nombresPairs(MonTableau))
